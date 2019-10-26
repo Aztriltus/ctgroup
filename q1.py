0b454b2a599dcc463f8082d60cf6c7cd94f40c3c
Ng Zheng Hao <zhenghao.ng.2016@sis.smu.edu.sg>
2019-10-26 22:30:11 +0800

    Added all_distances method to calculate distance between all points with all other points. Added origin into original tsp function by jx

--- q1.py
+++ q1.py
@@ -16,13 +16,28 @@ def list_reader(csv_file):
 
 #
 #
-# Algorithm main method here
+# Main method here
+# type either 'python q1.py' or 'python3 q1.py' to run this function
 #
 #
 def schedule_q1(orders=None, number_trucks=25):
-  # add in orders next time
-  d = cluster(25)
-  tsp(d[0]) 
+  # Remove this line when submitting
+  orders = list_reader('./orders.csv')
+
+  # Get clusters (dict)
+  # Need to change to a better clustering algo
+  d = cluster(orders, number_trucks)
+
+  # Testing with first group of clusters
+  all_distances = distances(d[0]) 
+  
+  # Grab the order with the smallest distance from origin
+  min_from_origin = min(all_distances['origin'], key=lambda k: all_distances['origin'][k])
+  # Get another order with shortest distance from either origin or min_from_origin
+  # Insert it into an array (that is sorted to determine the sequence of delivery)
+  # Repeat until all orders are in the array
+  # and return the array
+
   return
 
 #
@@ -31,9 +46,7 @@ def schedule_q1(orders=None, number_trucks=25):
 # Key=group_number and value='array of orders'
 #
 #
-def cluster(number_trucks, orders=None):
-  # use orders argument next time
-  orders = list_reader('./orders.csv')
+def cluster(orders, number_trucks):
 
   # Grab only the coordinates
   array = []
@@ -64,7 +77,55 @@ def cluster(number_trucks, orders=None):
 # 
 #
 # Accepts an array of orders 
-# Returns optimised route 
+# Returns dictionary that has the same length as orders
+# Key='order id' and value = dict('other order id': distance from original key)
+# Modified from tsp function written by JX
+#
+#
+def distances(orders):
+    distance_list = {}
+    print(orders)
+
+    for orderA in orders:
+#         distance = ((orderA[1] - 0 )**2 + (orderA[2] - 0)**2)** 0.5
+#         distance  = round(distance, 5)
+#         distance_list['Origin' + '-' + orderA[0]] = distance
+
+        for orderB in orders:
+            if orderA[0] != orderB[0]:
+                # Convert coordinates from String back to Float
+                ax, ay = float(orderA[1]), float(orderA[2])
+                bx, by = float(orderB[1]), float(orderB[2])
+
+                # Calculate distance
+                distance = ((ax - bx)**2 + (ay - by)**2)** 0.5
+                distance  = round(distance, 5)
+
+                # For each order, find the distance
+                if orderA[0] in distance_list:
+                  d_in_d = distance_list[orderA[0]]
+                  d_in_d[orderB[0]] = distance
+                else:
+                  d_in_d = {orderB[0]: distance}
+                  distance_list[orderA[0]] = d_in_d
+
+        # Added this to include distance between origin and all other orders
+        distance = ((0 - ax)**2 + (0 - ay)**2)** 0.5
+        distance  = round(distance, 5)
+        if 'origin' in distance_list:
+          d_in_d = distance_list['origin']
+          d_in_d[orderA[0]] = distance
+        else: 
+          d_in_d = {orderA[0]: distance}
+          distance_list['origin'] = d_in_d
+
+    return distance_list  
+
+# 
+#
+# Accepts an array of orders 
+# Returns distance of pairs that is sorted in asc 
+# Written by JX
 #
 #
 def tsp(orders):
@@ -84,8 +145,12 @@ def tsp(orders):
                 distance = ((ax - bx)**2 + (ay - by)**2)** 0.5
                 distance  = round(distance, 5)
                 distance_list[orderA[0] + '-' + orderB[0]] = distance
-                
+        distance = ((0 - ax)**2 + (0 - ay)**2)** 0.5
+        distance  = round(distance, 5)
+        distance_list['origin-' + orderA[0]] = distance
 
+                
+    # Added this to include distance between origin and all other orders
     for distanceA in distance_list.copy().keys():
         for distanceB in distance_list.copy().keys():
             if distanceA.split('-') == distanceB.split('-')[::-1]:
@@ -94,13 +159,6 @@ def tsp(orders):
     
     print()
     print(distance_list)
-    
-    # import random 
-    # route = []
-    # for truck_no in range(number_trucks):
-    #     number_truck = []
-        
-    #     number_truck.append 
-#     return(distance_list)       
+    return distance_list
 
 schedule_q1()
