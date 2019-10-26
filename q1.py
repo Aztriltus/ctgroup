bb2e7db9ddbda6c122fb3cd066eca96ec9baed74
Ng Zheng Hao <zhenghao.ng.2016@sis.smu.edu.sg>
2019-10-26 19:53:48 +0800

    Separated logic into functions. Cluster returns a dict. TSP is still WIP.

--- q1.py
+++ q1.py
@@ -1,9 +1,11 @@
-# 
-# <Team members' names>
+# Team ID??
+# Jiang Jia Xing, Ng Zheng Hao, Ong Soon Heng
 import numpy as np
 import csv
 from sklearn.cluster import AgglomerativeClustering
 
+# Reads CSV 
+# Remove when submitting
 def list_reader(csv_file):
     the_list = []
     with open(csv_file, 'r') as csvfile:
@@ -12,31 +14,62 @@ def list_reader(csv_file):
             the_list.append(row)
     return the_list
 
-# replace the content of this function with your own algorithm
+#
+#
+# Algorithm main method here
+#
+#
 def schedule_q1(orders=None, number_trucks=25):
-  orders = list_reader('./orders.csv')
-  d = {}
-  for order in orders:
-    d[order[0]] = [order[1],order[2]]
-  data = list(d.values())
-  X = np.array(data)
-  cluster = AgglomerativeClustering(n_clusters=number_trucks, affinity='euclidean', linkage='ward')
-  cluster.fit_predict(X)
-  print(cluster.labels_)
-	#return truck_paths
+  # add in orders next time
+  d = cluster(25)
+  tsp(d[0]) 
   return
 
-schedule_q1()
+#
+#
+# Returns dictionary 
+# Key=group_number and value='array of orders'
+#
+#
+def cluster(number_trucks, orders=None):
+  # use orders argument next time
+  orders = list_reader('./orders.csv')
 
+  # Grab only the coordinates
+  array = []
+  for order in orders:
+    array.append([order[1],order[2]])
 
+  # Convert into numpy array
+  X = np.array(array)
 
+  # Find clusters using sklearn
+  cluster = AgglomerativeClustering(n_clusters=number_trucks, affinity='euclidean', linkage='ward')
+  cluster.fit_predict(X)
 
+  # Create dictionary with
+  # key=group_no and value='array of orders' 
+  d = {}
+  index = 0
+  for group_no in cluster.labels_:
+    order = orders[index]
+    if group_no in d:
+      d[group_no].append(order)
+    else:
+      d[group_no] = [order]
+    index += 1
 
-##########################sorting of the distance#########################
-orders = [['O001', 5.5, 3.4], ['O002', -10.0, -8.9], ['O003', -1.9, -5.6]] 
-number_trucks = 5
-def schedule_q1(orders, number_trucks):
+  return d
+
+# 
+#
+# Accepts an array of orders 
+# Returns optimised route 
+#
+#
+def tsp(orders):
     distance_list = {}
+    print(orders)
 
     for orderA in orders:
 #         distance = ((orderA[1] - 0 )**2 + (orderA[2] - 0)**2)** 0.5
@@ -45,7 +78,10 @@ def schedule_q1(orders, number_trucks):
 
         for orderB in orders:
             if orderA[0] != orderB[0]:
-                distance = ((orderA[1] - orderB[1])**2 + (orderA[2] - orderB[2])**2)** 0.5
+                ax, ay = float(orderA[1]), float(orderA[2])
+                bx, by = float(orderB[1]), float(orderB[2])
+
+                distance = ((ax - bx)**2 + (ay - by)**2)** 0.5
                 distance  = round(distance, 5)
                 distance_list[orderA[0] + '-' + orderB[0]] = distance
                 
@@ -56,17 +92,15 @@ def schedule_q1(orders, number_trucks):
                 distance_list.pop(distanceA)
     distance_list = sorted(distance_list.items(), key=lambda x: x[1])
     
+    print()
     print(distance_list)
     
-    
-    
-    
-    import random 
-    route = []
-    for truck_no in range(number_trucks):
-        number_truck = []
+    # import random 
+    # route = []
+    # for truck_no in range(number_trucks):
+    #     number_truck = []
         
-        number_truck.append 
+    #     number_truck.append 
 #     return(distance_list)       
-#11111111111111111111
-#JXJXJX
+
+schedule_q1()
